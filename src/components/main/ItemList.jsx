a9f48cbd1f2a27289b2e14839f623c06d248c5bf
Camila Blanc Fick <camilaebf@gmail.com>
2022-02-17 22:28:43 -0300

    refactor grid css

--- src/components/main/ItemList.jsx
+++ src/components/main/ItemList.jsx
@@ -3,7 +3,7 @@ import Item from "./Item";
 
 
 export default function ItemList(props) {
-    return (<Row xs={2} md={3} xl={4} className="justify-content-center">
+    return (<Row xs={1} md={3} xl={4} className="justify-content-center">
         {props.loading ?
             (<Spinner animation="border" role="status" className="col-12">
                 <span className="visually-hidden">Loading...</span>

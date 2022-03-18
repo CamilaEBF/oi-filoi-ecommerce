2fe87f9732921b68bdd7ee9cbbaf2afc848cd091
Camila Blanc Fick <camilaebf@gmail.com>
2022-03-17 22:46:55 -0300

    add order

--- src/components/cart/BuyerForm.jsx
+++ src/components/cart/BuyerForm.jsx
@@ -6,20 +6,28 @@ export default function BuyerForm(props) {
         <h2>Formulario de pedido</h2>
         <p>Para finalizar tu pedido, completa los siguientes datos:</p>
         <Form onSubmit={props.onSubmitOrder} >
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextName">
+            <Form.Group as={Row} className="mb-3" controlId="name">
                 <Form.Label column sm="2">
                     Nombre y Apellido
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control plaintext readOnly defaultValue="Juan Pérez" />
+                    <Form.Control plaintext placeholder="Juan Pérez" />
                 </Col>
             </Form.Group>
-            <Form.Group as={Row} className="mb-3" controlId="formPlaintextPhoneNumber">
+            <Form.Group as={Row} className="mb-3" controlId="phone">
+                <Form.Label column sm="2">
+                    Número de teléfono
+                </Form.Label>
+                <Col sm="10">
+                    <Form.Control plaintext placeholder="+54 9 123-4567-891" />
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="email">
                 <Form.Label column sm="2">
                     Email
                 </Form.Label>
                 <Col sm="10">
-                    <Form.Control plaintext readOnly defaultValue="+54 9 123-4567-891" />
+                    <Form.Control plaintext placeholder="ejemplo@gmail.com" />
                 </Col>
             </Form.Group>
             <Button variant="primary" type="submit"> Enviar pedido </Button>
